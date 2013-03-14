cd56e15a8a412876345ae3fd72e4609b58d600c5
Will Moore <will@lifesci.dundee.ac.uk>
2013-03-14 23:45:21 +0000

    Use @login_required() to login. Show user name
    The @login_required() decorator attempts to retrieve an existing connection to OMERO using
    details in the http requrest. If it fails, you're redirected to webclient login, then back to
    this webgallery/ page on login. We simply show user name to indicate we're logged in

--- views.py
+++ views.py
@@ -1,10 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 
+from omeroweb.webclient.decorators import login_required
 
-def index(request):
+
+@login_required()
+def index(request, conn=None, **kwargs):
     """
     Just a place-holder while we get started
     """
 
-    return render_to_response("webgallery/index.html", {})
+    userName = conn.getUser().getFullName()
+
+    return render_to_response("webgallery/index.html", {'userName': userName})
