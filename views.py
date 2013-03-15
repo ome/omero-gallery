b62542f3f34ae9fe69f1f045182fcb2bd81111f3
Will Moore <will@lifesci.dundee.ac.uk>
2013-03-15 00:23:30 +0000

    Use @render_response() to reduce boilerplate code
    This decorator adds various data to the context that is passed to the template, E.g. ome.user
    so we don't have to include this in our views methods.

--- views.py
+++ views.py
@@ -1,18 +1,21 @@
 from django.http import HttpResponse
-from django.shortcuts import render_to_response
 
-from omeroweb.webclient.decorators import login_required
+from omeroweb.webclient.decorators import login_required, render_response
 
 
 @login_required()
+@render_response()
 def index(request, conn=None, **kwargs):
     """
     Just a place-holder while we get started
     """
 
-    userName = conn.getUser().getFullName()
     defaultGroup = conn.getGroupFromContext().getName()   # this is the 'default' group
 
     projects = conn.listProjects()      # Will be from ALL groups, owned by ALL users (as perms allow)
 
-    return render_to_response("webgallery/index.html", {'userName': userName, 'defaultGroup': defaultGroup, 'projects': projects})
+    context = {'template': "webgallery/index.html"}     # This is used by @render_response
+    context['defaultGroup'] = defaultGroup
+    context['projects'] = projects
+
+    return context
