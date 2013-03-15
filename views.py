d70c65766a0fd070a812937197b74324cf2d04a9
Will Moore <will@lifesci.dundee.ac.uk>
2013-03-15 00:56:43 +0000

    Use @login_required(setGroupContext=True) to filter by group
    Now, the 'conn' connection will be configured to the 'active_group' from the http request and
    by default, queries using conn will be filtered by the active group. Now listProjects() will
    only show Projects in the active group.

--- views.py
+++ views.py
@@ -3,19 +3,20 @@ from django.http import HttpResponse
 from omeroweb.webclient.decorators import login_required, render_response
 
 
-@login_required()
+@login_required(setGroupContext=True)
 @render_response()
 def index(request, conn=None, **kwargs):
     """
     Just a place-holder while we get started
     """
 
-    defaultGroup = conn.getGroupFromContext().getName()   # this is the 'default' group
+    groupId = conn.SERVICE_OPTS.getOmeroGroup()
+    active_group = conn.getObject("ExperimenterGroup", groupId).getName()
 
-    projects = conn.listProjects()      # Will be from ALL groups, owned by ALL users (as perms allow)
+    projects = conn.listProjects()      # Will be from active group, owned by ALL users (as perms allow)
 
     context = {'template': "webgallery/index.html"}     # This is used by @render_response
-    context['defaultGroup'] = defaultGroup
+    context['active_group'] = active_group
     context['projects'] = projects
 
     return context
