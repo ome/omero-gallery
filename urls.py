fcb1661721cce47e489f9fd4061cfc64e7b00155
Will Moore <will@lifesci.dundee.ac.uk>
2013-04-03 10:29:03 +0100

    Project view can 'load all thumbs' for a Dataset

--- urls.py
+++ urls.py
@@ -15,6 +15,11 @@ urlpatterns = patterns('django.views.generic.simple',
 
     # dataset view
     url( r'show_dataset/(?P<datasetId>[0-9]+)/$', views.show_dataset, name='webgallery_show_dataset' ),
+    # use the same dataset view, with a different template that only shows thumbnails
+    url( r'dataset_thumbs/(?P<datasetId>[0-9]+)/$',
+            views.show_dataset,
+            {'template': 'webgallery/dataset_thumbs.html'},
+            name='webgallery_dataset_thumbs' ),
 
     # image view
     url( r'show_image/(?P<imageId>[0-9]+)/$', views.show_image, name='webgallery_show_image' ),
