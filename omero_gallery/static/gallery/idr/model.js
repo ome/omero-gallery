06e1674dff119d299d39f00d3884e0bf82a5a9a1
William Moore <w.moore@dundee.ac.uk>
2019-05-04 22:32:49 +0100

    Fix issues with auto-complete. Case insensitive. Sort by best match

--- omero_gallery/static/gallery/idr/model.js
+++ omero_gallery/static/gallery/idr/model.js
@@ -24,6 +24,7 @@ StudiesModel.prototype.getStudyValue = function getStudyValue(study, key) {
 }
 
 StudiesModel.prototype.getKeyValueAutoComplete = function getKeyValueAutoComplete(key, inputText) {
+  inputText = inputText.toLowerCase();
   // Get values for key from each study
   let values = this.studies.map(study => {
     let v = this.getStudyValue(study, key);
@@ -33,23 +34,48 @@ StudiesModel.prototype.getKeyValueAutoComplete = function getKeyValueAutoComplet
   });
   // We want values that match inputText
   // Except for "Publication Authors", where we want words
+  // Create dict of {lowercaseValue: origCaseValue}
   let matchCounts = values.reduce((prev, value) => {
     let matches = [];
     if (key == "Publication Authors") {
       let names = value.match(/\b(\w+)\b/g) || [];
-      matches = names.filter(name => name.indexOf(inputText) > -1);
-    } else if (value.indexOf(inputText) > -1) {
+      matches = names.filter(name => name.toLowerCase().indexOf(inputText) > -1);
+    } else if (value.toLowerCase().indexOf(inputText) > -1) {
       matches.push(value);
     }
     matches.forEach(match => {
-      if (!prev[match]) prev[match] = 0;
-      prev[match]++;
+      if (!prev[match.toLowerCase()]) {
+        prev[match.toLowerCase()] = match;
+      }
     });
 
     return prev;
   }, {});
 
-  return Object.keys(matchCounts);
+  // Make into list and sort by:
+  // match at start of phrase > match at start of word > other match
+  let matchList = [];
+  for (key in matchCounts) {
+    let matchScore = 1;
+    if (key.indexOf(inputText) == 0) {
+      // best match if our text STARTS WITH inputText
+      matchScore = 3;
+    } else if (key.indexOf(" " + inputText) > -1) {
+      // next best if a WORD starts with inputText
+      matchScore = 2;
+    }
+    // Make a list of sort score and orig text (NOT lowercase keys)
+    matchList.push([matchScore, matchCounts[key]]);
+  }
+
+  // Sort by the matchScore
+  matchList.sort(function(a, b) {
+    return (a[0] < b[0] ? 1 :
+      a[0] > b[0] ? -1 : 0)
+  });
+
+  // Return the matches
+  return matchList.map(m => m[1]);
 }
 
 
