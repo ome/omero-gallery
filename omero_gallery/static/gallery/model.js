57603d16ef9767eb61351a256e14634134676d3b
William Moore <w.moore@dundee.ac.uk>
2019-05-25 06:45:13 +0100

    Sort auto-complete by lowerCase

--- omero_gallery/static/gallery/model.js
+++ omero_gallery/static/gallery/model.js
@@ -116,7 +116,7 @@ StudiesModel.prototype.getKeyValueAutoComplete = function getKeyValueAutoComplet
     if (a[0] < b[0]) return 1;
     if (a[0] > b[0]) return -1;
     // equal score. Sort by value (lowest first)
-    if (a[1] > b[1]) return 1;
+    if (a[1].toLowerCase() > b[1].toLowerCase()) return 1;
     return -1;
   });
 
