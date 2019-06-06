9872f3271c3cf94e15598f6926fcddd46c7a78c3
Sebastien Besson <seb.besson@gmail.com>
2019-06-06 09:42:03 +0100

    Bump version to next gallery pre-release

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
-VERSION = '3.2.0dev4'
+VERSION = '3.2.0a4'
 HOMEPAGE = "https://github.com/ome/omero-gallery"
 
 setup(name="omero-gallery",
