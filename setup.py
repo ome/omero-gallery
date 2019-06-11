22960ab157d917ca03d59bfa9e97c79d3908de7b
Simon Li <spli@dundee.ac.uk>
2019-06-11 10:58:43 +0100

    gallery pre-release 3.2.0.a6

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
-VERSION = '3.2.0.dev6'
+VERSION = '3.2.0.a6'
 HOMEPAGE = "https://github.com/ome/omero-gallery"
 
 setup(name="omero-gallery",
