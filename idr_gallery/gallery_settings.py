895d38a7a2315f9979bc5961f43eba67b05b9146
William Moore <w.moore@dundee.ac.uk>
2022-03-15 12:19:40 +0000

    Don't set base_url and gallery_index by default
    These aren't set on idr or idr-testing, so we don't want the default to be idr-testing

--- idr_gallery/gallery_settings.py
+++ idr_gallery/gallery_settings.py
@@ -29,16 +29,16 @@ GALLERY_SETTINGS_MAPPING = {
 
     "omero.web.gallery.base_url":
         ["BASE_URL",
-         "https://idr-testing.openmicroscopy.org",
+         None,
          str_slash,
          ("Base URL to use for non-gallery JSON AJAX requests."
-          " e.g. 'https://idr.openmicroscopy.org'."
+          " e.g. 'https://idr.openmicroscopy.org/'."
           " This allows data to be loaded from another OMERO server."
           " The default behaviour is to use the current server.")],
 
     "omero.web.gallery.gallery_index":
         ["GALLERY_INDEX",
-         "https://idr-testing.openmicroscopy.org/",
+         None,
          str_slash,
          ("Base gallery URL to use for gallery JSON AJAX requests."
           " e.g. 'https://idr.openmicroscopy.org/' is gallery index on IDR."
