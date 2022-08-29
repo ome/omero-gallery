1b19344fac7fd1639d8510852446e715760dda53
William Moore <w.moore@dundee.ac.uk>
2022-08-29 12:43:36 +0100

    Lazy load thumbnails

--- idr_gallery/static/idr_gallery/omero_search_form.js
+++ idr_gallery/static/idr_gallery/omero_search_form.js
@@ -469,7 +469,7 @@ class OmeroSearchForm {
         // Each humbnail links to image viewer. Hover menu links to viewer (eye) and webclient (i)
         return `<li class="studyThumb">
           <a target="_blank" href="${BASE_URL}webclient/img_detail/${img.id}">
-            <img title="${img.name}" src="${BASE_URL}webclient/render_thumbnail/${img.id}/" />
+            <img title="${img.name}" src="${BASE_URL}webclient/render_thumbnail/${img.id}/" loading="lazy" />
           </a>
           <ul class="imgLinks">
             <li title="Browse image metadata">
