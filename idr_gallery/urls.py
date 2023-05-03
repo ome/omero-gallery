4b766bc8940d5b6b1745459673b5289e7eae9940
William Moore <w.moore@dundee.ac.uk>
2023-05-03 11:11:43 +0100

    urls.py uses re_path

--- idr_gallery/urls.py
+++ idr_gallery/urls.py
@@ -1,26 +1,26 @@
-from django.conf.urls import url
+from django.urls import re_path
 from .gallery_settings import SUPER_CATEGORIES
 
 from . import views
 
 urlpatterns = [
     # index 'home page' of the idr_gallery app
-    url(r'^$', views.index, name='idr_gallery_index'),
+    re_path(r'^$', views.index, name='idr_gallery_index'),
 
     # All settings as JSON
-    url(r'^gallery_settings/$', views.gallery_settings),
+    re_path(r'^gallery_settings/$', views.gallery_settings),
 
     # Search page shows Projects / Screens filtered by Map Annotation
-    url(r'^search/$', views.index, {'super_category': None},
+    re_path(r'^search/$', views.index, {'super_category': None},
         name="idr_gallery_search"),
 
     # Supports e.g. ?project=1&project=2&screen=3
-    url(r'^gallery-api/thumbnails/$', views.api_thumbnails,
+    re_path(r'^gallery-api/thumbnails/$', views.api_thumbnails,
         name='idr_gallery_api_thumbnails'),
 ]
 
 for c in SUPER_CATEGORIES:
-    urlpatterns.append(url(r'^%s/$' % c, views.index, {'super_category': c},
+    urlpatterns.append(re_path(r'^%s/$' % c, views.index, {'super_category': c},
                            name="gallery_super_category"))
-    urlpatterns.append(url(r'^%s/search/$' % c, views.index,
+    urlpatterns.append(re_path(r'^%s/search/$' % c, views.index,
                            {'super_category': c}))
